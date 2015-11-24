ae65a4bdfa0e2f9dcbc76b376af26356f6cd6818
tht-krisztian <tht.krisztian@gmail.com>
2015-11-24 15:25:52 +0100

    Apró valtozasok
    Utility, operation függvények implementálása a teszt környezetbe kicsit
    problémás

---
+++ Robot_collision/src/Utility.java
@@ -0,0 +1,13 @@
+package robot_v1;
+
+public class Utility {
+	public boolean updateUp(){ return true;}
+	public boolean updateDown(){return false;}
+	public boolean updateLeft(){return true;}
+	public boolean updateRight(){return false;}
+	public long getStep(String path, long i){return i%4;}
+	
+	public long sendMessage(String s1){ return 1;}
+	public String waitMessage(){return "Cloud Online";}
+	public String getPath(){return "I'm Alive";}
+}
