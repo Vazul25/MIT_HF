d0900c3be66438512bae383afdb6d62a707651e7
tht-krisztian <tht.krisztian@gmail.com>
2015-12-07 19:41:18 +0100

    Working on Uppal transfer

--- Robot_collision/src/Utility.java
+++ Robot_collision/src/Utility.java
@@ -1,13 +1,41 @@
-package robot_v1;
-
 public class Utility {
-	public boolean updateUp(){ return true;}
-	public boolean updateDown(){return false;}
-	public boolean updateLeft(){return true;}
-	public boolean updateRight(){return false;}
+	
+	long n = 5;
+	boolean[] mtx = {	false,false,false,false,false,
+						false,true, true, true,false,
+						false,false,false,true,false,
+						false,false,false,false,false,
+						false,false,false,false,false	};
+	
+	long rand = 0;
+	
+	public boolean updateUp(long x, long y){ 
+		if(y == 0) return true;
+		return mtx[(int) (y*(n-1)+x)];
+	}
+	public boolean updateDown(long x, long y){
+		if(y == (n-1)) return true;
+		return mtx[(int)(y*(n+1)+x)];
+	}
+	public boolean updateLeft(long x, long y){
+		if(x == 0) return true;
+		return mtx[(int)(y*n+x-1)];
+	}
+	public boolean updateRight(long x, long y){
+		if(x == n-1) return true;
+		return mtx[(int)(y*n+x+1)];
+	}
 	public long getStep(String path, long i){return i%4;}
 	
-	public long sendMessage(String s1){ return 1;}
-	public String waitMessage(){return "Cloud Online";}
+	public long sendMessage(long s1){ return 1;}
+	public long waitMessage(){
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (rand++)%5;
+	}
 	public String getPath(){return "I'm Alive";}
 }
