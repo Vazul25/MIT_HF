2bd5083843e917db1abcf1f9e7f037a19bc7bfd1
tht-krisztian <tht.krisztian@gmail.com>
2015-12-08 17:17:08 +0100

    Apró változások

--- Robot_collision/src-gen/robot_interface.cpp
+++ Robot_collision/src-gen/robot_interface.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 enum Comm_Packets{
 	No_Message = 0, Cloud_Online = 1, Robot_Online = 2, Dead_End = 3, Path_sended = 4
-}
+};
 
-class Comm : public Robot_v1::SCI_Comm_OCB{
+class RobotComm : public Robot_v1::SCI_Comm_OCB{
 public:	
 	sc_integer sendMessage(sc_integer s1, sc_integer x = 0, sc_integer y = 0) {
 		ofstream myfile;
@@ -16,35 +16,58 @@ public:
 		if(myfile.is_open()){
 			myfile << s1 << " " << (int)x << " " << (int)y << endl;
 			myfile.close();
-			return 1
+			return 1;
 		}
 		else return 0;
-	};
+	}
 	
-
 	sc_integer waitMessage(){
 		ifstream myfile;
 		int output = 0;
-		if(myfile.open("msgFromCloud.txt")){
+		myfile.open("msgFromCloud.txt");
+		if(myfile.is_open()){
 			char message = '0';
 			
-			myfile.get(&message);
+			myfile.get(message);
 			if(message >= '0' && message <= '4'){
 				output = (int) (message-'0');
 			}
 			myfile.close();
 		}
 		return output;
-	};
+	}
+
+	sc_integer getInitX(){
+
+	}
+				
+	sc_integer getInitY(){
+
+	}
+	
+	sc_integer getMapX(){
+
+	}
+	
+	sc_integer getMapY(){
+
+	}
+	
+	sc_integer getGoalX(){
+
+	}
+	
+	sc_integer getGoalY(){
+
+	}
+				
 	
 	sc_string getPath(){
 
-	};
-
-
+	}
 };
 
-class SCI_Sensor_OCB {
+class RobotSensor : public SCI_Sensor_OCB {
 	public:		
 		sc_boolean updateUp(sc_integer x, sc_integer y) {
 
@@ -63,10 +86,25 @@ class SCI_Sensor_OCB {
 		}
 };
 
-class InternalSCI_OCB {
+class RobotUtility : public InternalSCI_OCB {
 public:
 	
 	sc_integer getStep(sc_string path, sc_integer i) {
 
 	}
 };
+
+int main()
+{
+	Robot_v1 robot_obj;
+	RobotComm robot_comm;
+	RobotSensor robot_sensor;
+	RobotUtility robot_util;
+
+	setSCI_Comm_OCB(&robot_comm);
+	setSCI_Sensor_OCB(&robot_sensor);
+	setInternalSCI_OCB(robot_util);
+
+	robot_obj
+  	return 0;
+}
